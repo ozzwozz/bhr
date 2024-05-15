5dfd9ca10a6808437efaba8dde827b64cf25621f
Oscar <61410630+ozzwozz@users.noreply.github.com>
2024-05-15 14:18:14 +0100

    doxygen fix

--- src/I2CDevice.cpp
+++ src/I2CDevice.cpp
@@ -1,11 +1,9 @@
 #include "I2CDevice.h"
 
 
- /// @brief Construct a new I2CDevice::I2CDevice object
- 
- /// @param i2c i2c instance
- /// @param address target device address
- 
+/// @brief Construct a new I2CDevice::I2CDevice object
+/// @param i2c i2c instance
+/// @param address target device address
 I2CDevice::I2CDevice(i2c_inst_t *i2c, uint8_t address)
     : m_i2c(i2c)
     , m_address(address)
@@ -13,20 +11,16 @@ I2CDevice::I2CDevice(i2c_inst_t *i2c, uint8_t address)
 }
 
 
- /// @brief Destroy the I2CDevice::I2CDevice object
- 
+/// @brief Destroy the I2CDevice::I2CDevice object
 I2CDevice::~I2CDevice()
 {
 }
 
-
- /// @brief Read datasent from the target device
- 
- /// @param data pointer to address of data to be sent
- /// @param len length of the data to be sent
- /// @return true 
- /// @return false 
- 
+/// @brief Read datasent from the target device
+/// @param data pointer to address of data to be sent
+/// @param len length of the data to be sent
+/// @return true 
+/// @return false 
 bool I2CDevice::read(uint8_t *data, size_t len)
 {
     bool success = false;
@@ -38,13 +32,11 @@ bool I2CDevice::read(uint8_t *data, size_t len)
 }
 
 
- /// @brief Write given data to the target device
- 
- /// @param data pointer to address of data to be sent
- /// @param len length of the data to be sent
- /// @return true 
- /// @return false 
- 
+/// @brief Write given data to the target device
+/// @param data pointer to address of data to be sent
+/// @param len length of the data to be sent
+/// @return true 
+/// @return false 
 bool I2CDevice::write(const uint8_t *data, size_t len)
 {
     bool success = false;
--- src/M24M02.cpp
+++ src/M24M02.cpp
@@ -1,31 +1,25 @@
 #include "M24M02.h"
 
 
- /// @brief Construct a new M24M02::M24M02 object
- 
- /// @param i2c 
- /// @param address 
- 
+/// @brief Construct a new M24M02::M24M02 object
+/// @param i2c 
+/// @param address 
 M24M02::M24M02(i2c_inst_t *i2c, uint8_t address) : I2CDevice(i2c, address)
 {
 }
 
 
- /// @brief Destroy the M24M02::M24M02 object
- 
+/// @brief Destroy the M24M02::M24M02 object
 M24M02::~M24M02()
 {
 }
 
-
- /// @brief Read from the EEPROM device
- 
- /// @param address register address to access
- /// @param data object to store the data in
- /// @param len length of the data to be sent and received
- /// @return true 
- /// @return false 
- 
+/// @brief Read from the EEPROM device
+/// @param address register address to access
+/// @param data object to store the data in
+/// @param len length of the data to be sent and received
+/// @return true 
+/// @return false 
 bool M24M02::read(const uint32_t address, uint8_t *data, size_t len)
 {
     bool success = true;
@@ -45,15 +39,12 @@ bool M24M02::read(const uint32_t address, uint8_t *data, size_t len)
     return success;
 }
 
-
- /// @brief Write to the EEPROM Device
- 
- /// @param address address to be written to 
- /// @param data data to write to the device
- /// @param len length of the data to be sent
- /// @return true 
- /// @return false 
- 
+/// @brief Write to the EEPROM Device
+/// @param address address to be written to 
+/// @param data data to write to the device
+/// @param len length of the data to be sent
+/// @return true 
+/// @return false 
 bool M24M02::write(const uint32_t address, const uint8_t *data, size_t len)
 {
     bool success = true;
